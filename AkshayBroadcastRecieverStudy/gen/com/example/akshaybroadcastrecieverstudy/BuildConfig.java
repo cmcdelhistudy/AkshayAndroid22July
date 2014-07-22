ee76f324c7d52676e5251b1531cfb01f2017f1d1
User <User@User-PC>
2014-07-22 09:42:41 +0530

    Android Study

---
+++ AkshayBroadcastRecieverStudy/gen/com/example/akshaybroadcastrecieverstudy/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.akshaybroadcastrecieverstudy;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
